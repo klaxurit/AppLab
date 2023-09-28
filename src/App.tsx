3ecc8925d58c398d1cc9c09b328c0d9e0979a1f5
Axurit <hjunca@skalv-studio.fr>
2023-09-28 15:52:27 +0200

    remove cosmoskit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { useEffect } from "react";
 import { setSelectedNetwork } from "./store/selectedNetworkSlice";
 import { setDevice } from './store/deviceSlice';
 import { Network } from "./types";
-import { useWalletClient } from "@cosmos-kit/react";
 import { setStatus, setData } from './store/walletSlice';
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./store";
@@ -17,7 +16,6 @@ function App() {
   const { data: networks, isLoading } = useGetNetworksQuery();
   const walletStatus = useSelector((state: RootState) => state.wallet.status);
   const walletData = useSelector((state: RootState) => state.wallet.data);
-  const { status, client } = useWalletClient("keplr-extension");
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -40,28 +38,6 @@ function App() {
       }
     }
   }, [isLoading, networks, dispatch]);
-
-  useEffect(() => {
-    dispatch(setStatus(status));
-    console.log("WALLETSTATUS: ", walletStatus, "WALLETDATA: ", walletData)
-    console.log("STATUS: ", status);
-    if (status === "Done") {
-      const fetchAccounts = async () => {
-        try {
-          const networks = ["cosmoshub-4", "osmosis-1", "juno-1"];
-          for (const network of networks) {
-            const account = await client?.getAccount?.(network);
-            if (account) {
-              dispatch(setData({ network, data: account }));
-            }
-          }
-        } catch (error) {
-          console.error("Error fetching account data", error);
-        }
-      };
-      fetchAccounts();
-    }
-  }, [status, client]);
 
   return (
     <div className="App">
